cea52cab7a8db9e32bf5c4a79831d7449dd2d52d
teudueududhs-sudo <teudueududhs@gmail.com>
2026-06-29 20:51:06 -0400

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,62 @@
+/* ======================================
+   NEZ ANIME
+   storage.js
+====================================== */
 
+const Storage = {
+
+    /* -----------------------------
+       Save Background
+    ----------------------------- */
+
+    saveBackground(image){
+
+        localStorage.setItem(
+            "nez_background",
+            image
+        );
+
+    },
+
+    getBackground(){
+
+        return localStorage.getItem(
+            "nez_background"
+        );
+
+    },
+
+    /* -----------------------------
+       Favorites
+    ----------------------------- */
+
+    saveFavorites(list){
+
+        localStorage.setItem(
+            "nez_favorites",
+            JSON.stringify(list)
+        );
+
+    },
+
+    getFavorites(){
+
+        return JSON.parse(
+            localStorage.getItem(
+                "nez_favorites"
+            )
+        ) || [];
+
+    },
+
+    addFavorite(anime){
+
+        let favorites = this.getFavorites();
+
+        favorites.push(anime);
+
+        this.saveFavorites(favorites);
+
+    },
+
+    removeFavorite
